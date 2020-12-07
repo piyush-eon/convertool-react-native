0c4504f276f8fda609804e0f13c2154edc063250
Piyush55dude <piyushagarwalvo@gmail.com>
2020-12-07 16:31:36 +0530

    Created all the tabs

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import convert from "convert-units";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { TabBar, TabView } from "react-native-tab-view";
+import Constants from "expo-constants";
+
+const measures = convert().measures();
+const primaryColor = "#052f5f";
+
+const MeasureView = ({ measure }) => <Text>{measure}</Text>;
+
+function unCamelCase(value) {
+  return value.replace(/([A-Z])/g, " $1");
+}
 
 export default function App() {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState(
+    measures.map((m) => ({ key: m, title: unCamelCase(m) }))
+  );
+
+  const renderScene = ({ route }) => {
+    return <MeasureView measure={route.key} />;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.scene, { marginTop: Constants.statusBarHeight }]}>
+      <Text style={styles.title}>unit Convertool</Text>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: Dimensions.get("window").width }}
+        renderTabBar={(props) => (
+          <TabBar
+            {...props}
+            scrollEnabled
+            tabStyle={{ width: "auto" }}
+            indicatorStyle={{ backgroundColor: "white" }}
+            style={{ backgroundColor: primaryColor }}
+          />
+        )}
+      ></TabView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  scene: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    padding: 15,
+    fontWeight: "bold",
+    color: primaryColor,
+    textAlign: "center",
+    fontSize: 20,
+    textTransform: "uppercase",
   },
 });
